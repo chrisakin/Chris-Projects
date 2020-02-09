62a778c336bafad6534e995be0e3ba116ca08e3e
chrisakin <52803463+chrisakin@users.noreply.github.com>
2020-02-09 13:14:28 +0100

    more components

--- Texi Travels/tex-travels/src/app/app.module.ts
+++ Texi Travels/tex-travels/src/app/app.module.ts
@@ -5,12 +5,19 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
+import { TitleHeaderComponent } from './title-header/title-header.component';
+
+import { HomeComponent } from './home/home.component';
+import { ViewPostComponent } from './view-post/view-post.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    FooterComponent
+    FooterComponent,
+    TitleHeaderComponent,
+    HomeComponent,
+    ViewPostComponent
   ],
   imports: [
     BrowserModule,
