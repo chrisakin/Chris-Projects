705d032a0af13fd1c192e967d038e35fd16adbe4
chrisakin <52803463+chrisakin@users.noreply.github.com>
2020-02-21 15:51:09 +0100

    about component and stylings

--- Chris Portfolio Using Angular/my-portfolio/src/app/app-routing.module.ts
+++ Chris Portfolio Using Angular/my-portfolio/src/app/app-routing.module.ts
@@ -2,7 +2,28 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 
-const routes: Routes = [];
+import { FrontPageComponent } from './front-page/front-page.component';
+import { AboutComponent } from './about/about.component';
+
+import { PortfolioComponent } from './portfolio/portfolio.component';
+import { ServicesComponent } from './services/services.component';
+import { ContactComponent } from './contact/contact.component';
+import { NoPageFoundComponent } from './no-page-found/no-page-found.component';
+import { PicturesComponent } from './pictures/pictures.component';
+
+path: "..."
+
+const routes: Routes = [
+  { path: '', redirectTo: '/home', pathMatch: 'full' },
+  { path: 'home', component: FrontPageComponent },
+  { path: 'about', component: AboutComponent },
+ 
+  { path: 'portfolio', component: PortfolioComponent },
+  { path: 'contact', component: ContactComponent },
+  { path: 'services', component: ServicesComponent },
+  { path: 'pictures', component: PicturesComponent },
+  { path: '**', component: NoPageFoundComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- Chris Portfolio Using Angular/my-portfolio/src/app/app.module.ts
+++ Chris Portfolio Using Angular/my-portfolio/src/app/app.module.ts
@@ -1,20 +1,41 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import { RouterModule, Routes} from '@angular/router';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { FrontPageComponent } from './front-page/front-page.component';
+import { AboutComponent } from './about/about.component';
+
+import { PortfolioComponent } from './portfolio/portfolio.component';
+import { ServicesComponent } from './services/services.component';
+import { ContactComponent } from './contact/contact.component';
+import { NoPageFoundComponent } from './no-page-found/no-page-found.component';
+import { PicturesComponent } from './pictures/pictures.component';
+import { FooterComponent } from './footer/footer.component';
+
+
+
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    FrontPageComponent
+    FrontPageComponent,
+    AboutComponent,
+    PortfolioComponent,
+    ServicesComponent,
+    ContactComponent,
+    NoPageFoundComponent,
+    PicturesComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    RouterModule,
+    
   ],
   providers: [],
   bootstrap: [AppComponent]
