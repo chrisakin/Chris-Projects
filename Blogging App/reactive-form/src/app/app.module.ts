1ed4f098c5e6cd340711cd0e2e6b58709e399a02
chrisakin <52803463+chrisakin@users.noreply.github.com>
2020-03-18 16:53:54 +0100

    reactive form

--- Blogging App/reactive-form/src/app/app.module.ts
+++ Blogging App/reactive-form/src/app/app.module.ts
@@ -3,14 +3,22 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BlogFormComponent } from './reative-form/blog-form/blog-form.component';
+import { ReactiveFormsModule} from '@angular/forms';
+import { FormsModule} from '@angular/forms';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BlogFormComponent,
+    
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ReactiveFormsModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
